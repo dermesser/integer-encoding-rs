0e8859a8d0b7e47b7181782303b781c629518f2b
Lewin Bormann <lewin@lewin-bormann.info>
2017-10-11 20:35:33 +0200

    fixed: Replace manual implementations with a macro.

--- src/fixed.rs
+++ src/fixed.rs
@@ -25,170 +25,35 @@ pub trait FixedInt: Sized + Copy {
     }
 }
 
-// usize is encoded as u64.
-impl FixedInt for usize {
-    fn required_space() -> usize {
-        8
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        (self as u64).encode_fixed(dst)
-    }
-
-    fn decode_fixed(src: &[u8]) -> usize {
-        u64::decode_fixed(src) as usize
-    }
-}
-
-impl FixedInt for u64 {
-    fn required_space() -> usize {
-        8
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<u64, [u8; 8]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> u64 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 8] = [0; 8];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
+macro_rules! impl_fixedint {
+    ($t:ty, $sz:expr) => {
+        impl FixedInt for $t {
+            fn required_space() -> usize {
+                $sz
+            }
+            fn encode_fixed(self, dst: &mut [u8]) {
+                assert_eq!(dst.len(), Self::required_space());
+                let encoded = unsafe { transmute::<&$t, &[u8; $sz]>(&self) };
+                dst.clone_from_slice(encoded);
+            }
+            fn decode_fixed(src: &[u8]) -> $t {
+                assert_eq!(src.len(), Self::required_space());
+                let mut decoded: $t = 0;
+                let cast = unsafe { transmute::<&mut $t, &mut [u8; $sz]>(&mut decoded) };
+                for i in 0..$sz {
+                    cast[i] = src[i];
+                }
+                decoded
+            }
         }
-
-        let dec = unsafe { transmute::<[u8; 8], u64>(arr) };
-        dec
     }
 }
 
-impl FixedInt for u32 {
-    fn required_space() -> usize {
-        4
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<u32, [u8; 4]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> u32 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 4] = [0; 4];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
-        }
-
-        let dec = unsafe { transmute::<[u8; 4], u32>(arr) };
-        dec
-    }
-}
-
-impl FixedInt for u16 {
-    fn required_space() -> usize {
-        2
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<u16, [u8; 2]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> u16 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 2] = [0; 2];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
-        }
-
-        let dec = unsafe { transmute::<[u8; 2], u16>(arr) };
-        dec
-    }
-}
-
-// isize is encoded as i64.
-impl FixedInt for isize {
-    fn required_space() -> usize {
-        8
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        (self as i64).encode_fixed(dst)
-    }
-
-    fn decode_fixed(src: &[u8]) -> isize {
-        u64::decode_fixed(src) as isize
-    }
-}
-
-impl FixedInt for i64 {
-    fn required_space() -> usize {
-        8
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<i64, [u8; 8]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> i64 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 8] = [0; 8];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
-        }
-
-        let dec = unsafe { transmute::<[u8; 8], i64>(arr) };
-        dec
-    }
-}
-
-impl FixedInt for i32 {
-    fn required_space() -> usize {
-        4
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<i32, [u8; 4]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> i32 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 4] = [0; 4];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
-        }
-
-        let dec = unsafe { transmute::<[u8; 4], i32>(arr) };
-        dec
-    }
-}
-
-impl FixedInt for i16 {
-    fn required_space() -> usize {
-        2
-    }
-
-    fn encode_fixed(self, dst: &mut [u8]) {
-        assert_eq!(dst.len(), Self::required_space());
-        let enc = unsafe { transmute::<i16, [u8; 2]>(self) };
-        dst.clone_from_slice(&enc);
-    }
-    fn decode_fixed(src: &[u8]) -> i16 {
-        assert_eq!(src.len(), Self::required_space());
-        let mut arr: [u8; 2] = [0; 2];
-
-        for i in 0..src.len() {
-            arr[i] = src[i];
-        }
-
-        let dec = unsafe { transmute::<[u8; 2], i16>(arr) };
-        dec
-    }
-}
+impl_fixedint!(usize, 8);
+impl_fixedint!(u64, 8);
+impl_fixedint!(u32, 4);
+impl_fixedint!(u16, 2);
+impl_fixedint!(isize, 8);
+impl_fixedint!(i64, 8);
+impl_fixedint!(i32, 4);
+impl_fixedint!(i16, 2);
