6476b18e143d06a31d61c255e4cadeb806f504ed
Lewin Bormann <lewin@lewin-bormann.info>
2020-02-22 14:26:51 +0100

    Update example to be more robust

--- examples/read-write-file/src/main.rs
+++ examples/read-write-file/src/main.rs
@@ -4,25 +4,27 @@ use std::io;
 use integer_encoding::*;
 
 fn write_test_file() -> io::Result<()> {
+    let _ = fs::remove_file("/tmp/varintbytes");
     let mut f = fs::OpenOptions::new()
         .create(true)
         .write(true)
         .open("/tmp/varintbytes")?;
-    f.write_varint(30)?;
-    f.write_varint(60)?;
-    f.write_varint(90)?;
-    f.write_varint(9000000)?;
+    f.write_varint(30 as u32)?;
+    f.write_varint(60 as u32)?;
+    f.write_varint(90 as u32)?;
+    f.write_varint(9000000 as u32)?;
     Ok(())
 }
 
 async fn read_and_verify() -> io::Result<()> {
     let mut f = tokio::fs::File::open("/tmp/varintbytes").await?;
-    let i1: i32 = f.read_varint_async().await?;
-    let i2: i32 = f.read_varint_async().await?;
-    let i3: i32 = f.read_varint_async().await?;
-    let i4: i32 = f.read_varint_async().await?;
+    let i1: u32 = f.read_varint_async().await?;
+    let i2: u32 = f.read_varint_async().await?;
+    let i3: u32 = f.read_varint_async().await?;
+    let i4: u32 = f.read_varint_async().await?;
     assert!(f.read_varint_async::<u32>().await.is_err());
     println!("{:?}", (i1, i2, i3, i4));
+    assert!(i2 == 2 * i1 && i3 == 3 * i1);
     Ok(())
 }
 
